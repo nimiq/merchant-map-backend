743ec5cd99915630638172c5c0bc43a094c3173a
Walter Marchand <mail@waltermarchand.nl>
2021-12-11 20:16:12 +0100

    update form

--- resources/views/shops/create.blade.php
+++ resources/views/shops/create.blade.php
@@ -6,65 +6,69 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <form action="{{ route('shops.store') }}" method="POST">
                         @csrf
-                        <div class="flex">
-                            <label  class="w-24">Label </label>
-                            <input name="label" class="border">
+                        <h2 class="mb-4">{{ __('Create New Shop') }}</h2>
+                        <div class="mb-4">
+                            <x-label for="label">{{ __('Label') }}</x-label>
+                            <x-input id="label" name="label" class="border w-full" :value="old('label')" />
                         </div>
-                        <div class="flex">
-                            <label class="w-24">description</label>
-                            <input name="description" class="border">
+                        <div class="mb-4">
+                            <x-label for="description">{{ __('Description') }}</x-label>
+                            <x-input id="description" name="description" class="border w-full" :value="old('description')" />
                         </div>
-                        <div class="flex">
-                            <label class="w-24">object_id</label>
-                            <input name="object_id" class="border">
+                        <h2></h2>
+                        <div class="mb-4">
+                            <x-label for="object_id">{{ __('Object ID') }}</x-label>
+                            <x-input id="object_id" name="object_id" class="border w-full" :value="old('object_id')" />
                         </div>
-                        <div class="flex">
-                            <label class="w-24">source_id</label>
-                            <input name="source_id" class="border">
+                        <div class="mb-4">
+                            <x-label for="source_id">{{ __('Source ID') }}</x-label>
+                            <x-input id="source_id" name="source_id" class="border w-full" :value="old('source_id')" />
                         </div>
-                        <div class="flex">
-                            <label class="w-24">website</label>
-                            <input name="website" class="border">
+                        <h2 class="mb-4">{{ __('Contact Information') }}</h2>
+                        <div class="mb-4">
+                            <x-label>{{ __('Website URL') }}</x-label>
+                            <x-input id="website" name="website" class="border w-full" :value="old('website')" />
                         </div>
-                        <div class="flex">
-                            <label class="w-24">email</label>
-                            <input name="email" class="border">
+                        <div class="mb-4">
+                            <x-label for="email">{{ __('Email') }}</x-label>
+                            <x-input id="email" name="email" class="border w-full" :value="old('email')" />
                         </div>
-                        <div class="flex">
-                            <label class="w-24">phone</label>
-                            <input name="phone" class="border">
+                        <div class="mb-4">
+                            <x-label for="phone">{{ __('Phone') }}</x-label>
+                            <x-input id="phone" name="phone" class="border w-full" :value="old('phone')" />
                         </div>
-                        <div class="flex">
-                            <label class="w-24">street</label>
-                            <input name="street" class="border">
+                        <h2 class="mb-4">{{ __('Address') }}</h2>
+                        <div class="mb-4 flex gap-1">
+                            <div class="w-3/4">
+                                <x-label for="street">{{ __('Street') }}</x-label>
+                                <x-input id="street" name="street" class="border w-full" :value="old('street')" />
+                            </div>
+                            <div class="w-1/4">
+                                <x-label for="number">{{ __('Number') }}</x-label>
+                                <x-input id="number" name="number" class="border w-full" :value="old('number')" />
+                            </div>
                         </div>
-                        <div class="flex">
-                            <label class="w-24">number</label>
-                            <input name="number" class="border">
+                        <div class="mb-4 flex gap-1">
+                            <div class="w-1/4">
+                                <x-label for="zip">{{ __('Zip') }}</x-label>
+                                <x-input id="zip" name="zip" class="border w-full" :value="old('zip')" />
+                            </div>
+                            <div class="w-3/4">
+                                <x-label for="city">{{ __('City') }}</x-label>
+                                <x-input id="city" name="city" class="border w-full" :value="old('city')"/>
+                            </div>
                         </div>
-                        <div class="flex">
-                            <label class="w-24">zip</label>
-                            <input name="zip" class="border">
+                        <div class="mb-4">
+                            <x-label for="country">{{ __('Country') }}</x-label>
+                            <x-input id="country" name="country" class="border w-full" :value="old('country')"/>
                         </div>
-                        <div class="flex">
-                            <label class="w-24">city</label>
-                            <input name="city" class="border">
-                        </div>
-                        <div class="flex">
-                            <label class="w-24">country</label>
-                            <input name="country" class="border">
-                        </div>
-                        <div class="flex">
-                            <label class="w-24">digital_goods</label>
-                            <input type="checkbox" name="digital_goods" class="border">
-                        </div>
-                        <div class="flex">
-                            <button type="submit" class="border border-blue-400">Submit</button>
+                        <div class="flex flex-row-reverse">
+                            <x-button type="submit">Submit</x-button>
                         </div>
                     </form>
                 </div>
