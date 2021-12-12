50f3b0ec4136582cb1bfa354d434c75d9fdc91d9
Stefan <nast3zz@gmail.com>
2021-12-12 13:17:26 +0100

    Rename LocationController to ShippingController

--- app/Http/Controllers/ShippingController.php
+++ app/Http/Controllers/ShippingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class LocationController extends Controller
+class ShippingController extends Controller
 {
     /**
      * Display a listing of the resource.
