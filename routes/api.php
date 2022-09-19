2d24740b021c76ca789f66c897980a6c3022ba06
Jeffrey Esquivel S <jeff@nimiq.com>
2022-09-18 20:48:46 -0600

    API endpoint for getting issue categories

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/search', [ShopController::class, 'search']);
 Route::post('/issue', [IssueController::class, 'store']);
+
+Route::get('/issue_categories', function () {
+    // We only want to provide categories' id and label, the rest is just noise
+    return \App\Models\IssueCategory::all()->transform(function ($category) {
+        return array('id' => $category->id, 'label' => $category->label);
+    });
+});
